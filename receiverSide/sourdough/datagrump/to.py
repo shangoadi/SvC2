007a0b062785f07e8038dd19fcd028dcd594d67b
shangoadi <shangoadi@gmail.com>
2016-12-19 12:13:54 -0500

    More clean up

--- receiverSide/sourdough/datagrump/to.py
+++
@@ -1,9 +0,0 @@
-from subprocess import Popen, PIPE
-from sys import argv
-
-input = ' '.join(argv[1:])
-if not input: input = "no arguments given"
-cproc = Popen("./sender", stdin=PIPE, stdout=PIPE)
-out, err = cproc.communicate(input)
-print "output:", out
-print "errors:", err
